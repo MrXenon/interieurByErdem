0bc702e6c0be33744c85e5646d0f3b844d083cd2
SJ4MPIE <stieman@student.scalda.nl>
2021-03-10 15:17:42 +0100

    CTA.php toegevoegd aan services

--- services.php
+++ services.php
@@ -16,95 +16,98 @@ Onze service dienst zet graag uw gekochte product voor u in elkaar.">
 
 </head>
 <?php include 'elements/gdpr.php'; ?>
+
 <body class="stretched">
 
-<!-- Document Wrapper
+    <!-- Document Wrapper
 ============================================= -->
-<div id="wrapper" class="clearfix">
+    <div id="wrapper" class="clearfix">
 
-    <!--Include navigation-->
-    <?php include 'elements/nav.php';?>
+        <!--Include navigation-->
+        <?php include 'elements/nav.php'; ?>
 
 
-    <!-- Slider
+        <!-- Slider
    ============================================= -->
-    <section id="slider" class="slider-element dark swiper_wrapper slider-parallax min-vh-75">
-        <div class="slider-inner">
+        <section id="slider" class="slider-element dark swiper_wrapper slider-parallax min-vh-75">
+            <div class="slider-inner">
 
-            <div class="swiper-container swiper-parent">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide dark">
-                        <div class="container">
-                            <div class="slider-caption">
-                                <div>
-                                    <h2 class="nott" data-animate="fadeInUp">SERVICES</h2>
+                <div class="swiper-container swiper-parent">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide dark">
+                            <div class="container">
+                                <div class="slider-caption">
+                                    <div>
+                                        <h2 class="nott" data-animate="fadeInUp">SERVICES</h2>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="swiper-slide-bg" style="background: linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.5)), url('elements/slider/13.jpg') no-repeat center center; background-size: cover;"></div>
                         </div>
-                        <div class="swiper-slide-bg" style="background: linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.5)), url('elements/slider/13.jpg') no-repeat center center; background-size: cover;"></div>
                     </div>
                 </div>
+
             </div>
 
-        </div>
+        </section><!-- #page-title end -->
 
-    </section><!-- #page-title end -->
-
-    <!-- Content
+        <!-- Content
     ============================================= -->
-    <section id="content">
-        <div class="content-wrap py-0 overflow-visible" style="padding-top: 30px !important;">
+        <section id="content">
+            <div class="content-wrap py-0 overflow-visible" style="padding-top: 30px !important;">
 
-            <svg viewBox="0 0 1382 58" width="100%" height="60" preserveAspectRatio="none" style="position: absolute; top: -58px; left:0; z-index: 1"><path style="fill:#FFF;" d="M1.52.62s802.13,127,1380,0v56H.51Z"/></svg>
-            <div class="container">
-
-                <div class="row">
-                    <div class="col-lg-6">
-                        <h3>Klant is koning </h3>
-                        <div class="svg-line bottommargin-sm">
-                            <img src="demos/nonprofit/images/divider-1.svg" alt="svg divider" height="10">
-                        </div>
-                        <p>
-                            Interieur By Erdem onderscheidt zich door de klantgerichte aanpak en zeer persoonlijke benadering van de klant. “Schilderen, behangen, het aanbrengen van raamdecoratie, inclusief opmeten en ophangen van gordijnen doen we zelf.’’<br><br>
-                            Voor de vloeren en de buitenzonnewering hebben wij onze partners, eveneens vakmensen. Wij hebben een uitstekende band, allemaal een opleiding gevolgd en zijn gediplomeerd. Op school leerden we hoe alles te behandelen maar de kneepjes van het vak leer je uiteraard in de praktijk.
-
-                        </p>
-                    </div>
-
-                    <div class="col-lg-6">
-                        <h3>Ikram realiseert uw wens</h3>
-                        <div class="svg-line bottommargin-sm">
-                            <img src="demos/nonprofit/images/divider-1.svg" alt="svg divider" height="10">
-                        </div>
-                        <p>Ikram: Als mensen b.v. onzeker zijn over de aanschaf van een bepaald product of toepassing, of omdat ze niet weten wat ze precies nodig hebben, leggen wij uit welke keuze voor hen het beste is en wat ze precies nodig hebben, zonder af te dwingen natuurlijk. We staan altijd klaar voor de klant. Mochten wij op een vraag geen antwoord weten, gaan we te rade bij een externe specialist of vertegenwoordiger. Hier gaat niemand naar buiten zonder een oplossing, zelfs al vinden we die niet bij ons in de winkel. Je moet er altijd voor zorgen dat klanten terugkomen. Persoonlijk denk ik in de volledige breedte na voor onze klanten.
-
-                        </p>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="section parallax dark bottommargin-lg" style="background-image: url('elements/producten/IMG_8982.JPG'); background-size: cover; padding: 100px 0;" data-bottom-top="background-position:0px 0px;" data-top-bottom="background-position:0px -500px;">
-
-                <div class="container clearfix">
+                <svg viewBox="0 0 1382 58" width="100%" height="60" preserveAspectRatio="none" style="position: absolute; top: -58px; left:0; z-index: 1">
+                    <path style="fill:#FFF;" d="M1.52.62s802.13,127,1380,0v56H.51Z" />
+                </svg>
+                <div class="container">
 
                     <div class="row">
-                        <div class="col-lg-6 text-center" data-animate="bounceIn">
-                            <h3>Interieur By Erdem werkt vanuit het hart</h3>
+                        <div class="col-lg-6">
+                            <h3>Klant is koning </h3>
+                            <div class="svg-line bottommargin-sm">
+                                <img src="demos/nonprofit/images/divider-1.svg" alt="svg divider" height="10">
+                            </div>
                             <p>
-                                Er wordt hier gewerkt vanuit het hart; ons gevoel moet goed zijn. Ook de doe-het-zelver wordt bij ons met veel plezier geholpen en van advies voorzien.
-                                Bij Interieur By Erdem letten we goed op, zodat we van elkaars doen en laten op de hoogte zijn.
-                                Ik schrijf altijd alles op, verzorg de administratie en zorg ervoor dat afspraken keurig worden nagekomen.<br><br>
-                                Een snelle aflevering/afwikkeling, als iemand iets besteld heeft of wat gedaan wil hebben, vinden wij ook belangrijk. De klant is nu eenmaal koning!
+                                Interieur By Erdem onderscheidt zich door de klantgerichte aanpak en zeer persoonlijke benadering van de klant. “Schilderen, behangen, het aanbrengen van raamdecoratie, inclusief opmeten en ophangen van gordijnen doen we zelf.’’<br><br>
+                                Voor de vloeren en de buitenzonnewering hebben wij onze partners, eveneens vakmensen. Wij hebben een uitstekende band, allemaal een opleiding gevolgd en zijn gediplomeerd. Op school leerden we hoe alles te behandelen maar de kneepjes van het vak leer je uiteraard in de praktijk.
+
                             </p>
                         </div>
 
-                        <div class="col-lg-3 text-center" data-animate="bounceIn" data-delay="200">
-                            <h4>Ons uitgangspunt</h4>
-                            <blockquote class="parallax-quote">
-                                ‘Hoe zou ik zelf benaderd willen worden?’.
-                            </blockquote>
+                        <div class="col-lg-6">
+                            <h3>Ikram realiseert uw wens</h3>
+                            <div class="svg-line bottommargin-sm">
+                                <img src="demos/nonprofit/images/divider-1.svg" alt="svg divider" height="10">
+                            </div>
+                            <p>Ikram: Als mensen b.v. onzeker zijn over de aanschaf van een bepaald product of toepassing, of omdat ze niet weten wat ze precies nodig hebben, leggen wij uit welke keuze voor hen het beste is en wat ze precies nodig hebben, zonder af te dwingen natuurlijk. We staan altijd klaar voor de klant. Mochten wij op een vraag geen antwoord weten, gaan we te rade bij een externe specialist of vertegenwoordiger. Hier gaat niemand naar buiten zonder een oplossing, zelfs al vinden we die niet bij ons in de winkel. Je moet er altijd voor zorgen dat klanten terugkomen. Persoonlijk denk ik in de volledige breedte na voor onze klanten.
+
+                            </p>
                         </div>
+                    </div>
+
+                </div>
+
+                <div class="section parallax dark bottommargin-lg" style="background-image: url('elements/producten/IMG_8982.JPG'); background-size: cover; padding: 100px 0;" data-bottom-top="background-position:0px 0px;" data-top-bottom="background-position:0px -500px;">
+
+                    <div class="container clearfix">
+
+                        <div class="row">
+                            <div class="col-lg-6 text-center" data-animate="bounceIn">
+                                <h3>Interieur By Erdem werkt vanuit het hart</h3>
+                                <p>
+                                    Er wordt hier gewerkt vanuit het hart; ons gevoel moet goed zijn. Ook de doe-het-zelver wordt bij ons met veel plezier geholpen en van advies voorzien.
+                                    Bij Interieur By Erdem letten we goed op, zodat we van elkaars doen en laten op de hoogte zijn.
+                                    Ik schrijf altijd alles op, verzorg de administratie en zorg ervoor dat afspraken keurig worden nagekomen.<br><br>
+                                    Een snelle aflevering/afwikkeling, als iemand iets besteld heeft of wat gedaan wil hebben, vinden wij ook belangrijk. De klant is nu eenmaal koning!
+                                </p>
+                            </div>
+
+                            <div class="col-lg-3 text-center" data-animate="bounceIn" data-delay="200">
+                                <h4>Ons uitgangspunt</h4>
+                                <blockquote class="parallax-quote">
+                                    ‘Hoe zou ik zelf benaderd willen worden?’.
+                                </blockquote>
+                            </div>
 
                         </div>
                     </div>
@@ -168,7 +171,7 @@ Onze service dienst zet graag uw gekochte product voor u in elkaar.">
                                     <div class="feature-box fbox-plain bg-white mx-0">
                                         <div class="fbox-media position-relative col-auto p-0 mr-4">
                                             <a href="advies.php">
-                                            <img src="demos/nonprofit/images/icons/assigment.svg" alt="Featured Icon 1" width="50">
+                                                <img src="demos/nonprofit/images/icons/assigment.svg" alt="Featured Icon 1" width="50">
                                             </a>
                                         </div>
                                         <div class="fbox-content">
@@ -182,7 +185,7 @@ Onze service dienst zet graag uw gekochte product voor u in elkaar.">
                                     <div class="feature-box fbox-plain bg-white mx-0">
                                         <div class="fbox-media position-relative col-auto p-0 mr-4">
                                             <a href="kleur-en-verf-advies.php">
-                                            <img src="demos/nonprofit/images/icons/color-palette.svg" alt="Featured Icon 2" width="50">
+                                                <img src="demos/nonprofit/images/icons/color-palette.svg" alt="Featured Icon 2" width="50">
                                             </a>
                                         </div>
                                         <div class="fbox-content">
@@ -196,7 +199,7 @@ Onze service dienst zet graag uw gekochte product voor u in elkaar.">
                                     <div class="feature-box fbox-plain bg-white mx-0">
                                         <div class="fbox-media position-relative col-auto p-0 mr-4">
                                             <a href="materialen.php">
-                                            <img src="demos/nonprofit/images/icons/brush.svg" alt="Featured Icon 3" width="50">
+                                                <img src="demos/nonprofit/images/icons/brush.svg" alt="Featured Icon 3" width="50">
                                             </a>
                                         </div>
                                         <div class="fbox-content">
@@ -209,7 +212,7 @@ Onze service dienst zet graag uw gekochte product voor u in elkaar.">
                                     <div class="feature-box fbox-plain bg-white mx-0">
                                         <div class="fbox-media position-relative col-auto p-0 mr-4">
                                             <a href="services.php">
-                                            <img src="demos/nonprofit/images/icons/technical-support.svg" alt="Featured Icon 4" width="50">
+                                                <img src="demos/nonprofit/images/icons/technical-support.svg" alt="Featured Icon 4" width="50">
                                             </a>
                                         </div>
                                         <div class="fbox-content">
@@ -220,38 +223,25 @@ Onze service dienst zet graag uw gekochte product voor u in elkaar.">
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="section" style="padding: 30px 0; margin: 0 0; background-color: #D7D4C8;">
-            <div class="container-flex clearfix">
-                <div class="row justify-content-center">
-                    <div class="col-md-1"></div>
-                    <div class="col-md-6">
-                        <div class="heading-block border-bottom-0 mb-4">
-                            <h3 class="mb-4 nott" style="padding-left:50px;">Benieuwd naar wat Interieur By Erdem<br> voor u kan betekenen?</h3>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <a href="contact.php" class="btn btn-dark">Neem vrijblijvend contact op </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section><!-- #content end -->
+            <?php include 'cta.php'; ?>
+        </section><!-- #content end -->
+
+        <?php
+        include 'elements/footer.php';
+        include 'elements/floating-contact.php';
+        ?>
+
+
+
+    </div><!-- #wrapper end -->
 
     <?php
-    include 'elements/footer.php';
-    include 'elements/floating-contact.php';
+    include 'elements/footer-scripts.php';
     ?>
-
-
-
-</div><!-- #wrapper end -->
-
-<?php
-include 'elements/footer-scripts.php';
-?>
 </body>
+
 </html>
